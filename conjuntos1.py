ba82e5bb2d4bab318902f884eb70200cf914923e
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-15 18:04:10 -0300

    Add files via upload

---
+++ conjuntos1.py
@@ -0,0 +1,18 @@
+#Inserindo um conjunto
+A={1,2,3,4,5}
+Bruno={'Casa','Terreno','Carro'}
+
+#Imprimindo um conjunto (Forma 1)
+print(A)
+print(Bruno)
+
+#Imprimindo um conjunto (Forma 2)
+print(f'A={A}')
+print(f'Os bens de Bruno são {Bruno}.')
+
+#Relação de Pertinência
+print(2 in A)
+print(6 in A)
+print('Casa' in Bruno)
+print('Moto' in Bruno)
+
