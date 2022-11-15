3bd72a457de602db5143fced948a41bcb6d43249
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-15 18:06:23 -0300

    Add files via upload

---
+++ conjuntos4.py
@@ -0,0 +1,16 @@
+A={1,2,3,4,5}
+B={2,3,4,5,6,7}
+print(f'A={A}')
+
+#Adicionando um elemento a um conjunto
+A.add(6)
+print(f'A={A}')
+
+#Removendo um elemento de um conjunto
+B.remove(4)
+print(f'B={B}')
+
+#Apagando um elemento de um conjunto
+A.clear()
+A.add(10)
+print(f'A={A}')
