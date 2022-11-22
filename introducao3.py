8f1d54a6eb68df0e11420559ad7efc9bd7cd5c86
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-22 10:02:24 -0300

    Add files via upload

---
+++ introducao3.py
@@ -0,0 +1,6 @@
+# Utilização da biblioteca de Computação Científica numpy
+# Antes de executar, digite no Terminal o comando pip install numpy (isto instala a biblioteca numpy)
+
+import numpy
+x=numpy.cos(0)
+print(x)
---
+++ introducao3_1.py
@@ -0,0 +1,3 @@
+import numpy as np
+x=np.cos(0)
+print(x)
