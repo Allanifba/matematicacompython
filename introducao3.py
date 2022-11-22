592141a502b6dcc2ae01248c408615b1a8f5eb3d
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-22 10:02:08 -0300

    Delete introducao3.py

--- introducao3.py
+++
@@ -1,6 +0,0 @@
-# Utilização da biblioteca de Computação Científica numpy
-# Antes de executar, digite no Terminal o comando pip install numpy (isto instala a biblioteca numpy)
-
-import numpy as np
-x=np.cos(0)
-print(x)
