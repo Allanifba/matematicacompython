59167063fd746bf7e234bd1c35bd22d2639d2cc8
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-28 19:20:57 -0300

    Add files via upload

---
+++ estrutura_while.py
@@ -0,0 +1,6 @@
+# Estrutura de repetição while
+
+contador = 0
+while (contador<=10):
+    print(contador)
+    contador = contador + 1
---
+++ estrutura_while_else.py
@@ -0,0 +1,8 @@
+# Estrutura de repetição while-else
+
+contador = 0
+while (contador<=10):
+    print(contador)
+    contador = contador + 1
+else:
+    print('O loop terminou.')
