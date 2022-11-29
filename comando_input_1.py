ecbc64b68ac085cf6ab1752f9b812fa2044919c9
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-29 09:24:29 -0300

    Add files via upload

---
+++ comando_input_1.py
@@ -0,0 +1,5 @@
+# Comando input
+
+nome = input('Digite o nome do aniversariante: ')
+mes = input('Digite o mês de aniversário: ')
+print(f'{nome} faz aniversário no mês de {mes}.')
---
+++ comando_input_2.py
@@ -0,0 +1,4 @@
+# Comando input
+
+x = float(input('Digite um número: '))
+print(f'O quadrado de {x} é {x**2}.')
