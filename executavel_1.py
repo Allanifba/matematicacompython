29cc1b6365922fc23a99f92953d7812a08ebf2d9
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-11-29 10:37:46 -0300

    Add files via upload

---
+++ executavel_1.py
@@ -0,0 +1,10 @@
+# Gerando um arquivo executável (exe)
+
+nome = input('Digite o nome do aniversariante: ')
+mes = input('Digite o mês de aniversário: ')
+print(f'{nome} faz aniversário no mês de {mes}.')
+input('')
+
+# No Terminal execute os códigos:
+# (1) pip install pyinstaller
+# (2) pyinstaller --onefile executavel_1.py
---
+++ executavel_2.py
@@ -0,0 +1,7 @@
+# Gerando um arquivo executável (exe)
+
+while True:
+    nome = input('Digite o nome do aniversariante: ')
+    mes = input('Digite o mês de aniversário: ')
+    print(f'{nome} faz aniversário no mês de {mes}.')
+    print('')
