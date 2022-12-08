4835582c09da6132556e68c9965ca416a3421cff
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-12-08 12:33:56 -0300

    Add files via upload

---
+++ funcao_exponencial_1.py
@@ -0,0 +1,11 @@
+# Função exponencial
+
+from math import e
+a = e # base
+x = 1 # expoente
+
+def f(a,x):
+    return a**x
+
+y=f(a,x)
+print(y)
---
+++ funcao_exponencial_2.py
@@ -0,0 +1,7 @@
+# Função exponencial de base e
+
+from math import exp
+
+x = 2
+y = exp(x) # e**x
+print(y)
