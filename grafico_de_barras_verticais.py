bef67f96905ebd2a18779afe3169e069a5495730
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-12-18 09:56:17 -0300

    Add files via upload

---
+++ grafico_de_barras_verticais.py
@@ -0,0 +1,14 @@
+# Gráfico do tipo barras verticais
+
+import matplotlib.pyplot as plt
+
+classes = ['Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo']
+frequencias = [30,25,22,23,40,42,35]
+
+plt.bar(classes,frequencias,width = 0.9,color = 'blue',edgecolor = 'red')
+plt.title('Ocorrências de Pessoas na Emergência de um Hospital', fontsize = 15, color = 'black')
+plt.xlabel('Dia da Semana', fontsize = 10, color = 'black')
+plt.ylabel('Número de Ocorrências', fontsize = 10, color = 'black')
+plt.tick_params(labelsize=10)
+
+plt.show()
